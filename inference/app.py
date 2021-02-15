5c91d8cf24332f04475af6d86e37f16743456ce1
Marek Kuczynski <marek.kuczynski@gmail.com>
2021-02-15 23:49:41 +0100

    Update app.py

--- inference/app.py
+++ inference/app.py
@@ -16,10 +16,13 @@ def handler(event, context):
     message = message_bytes.decode('ascii')
 
     # analyse sentiment of the submitted text
-    x = pipeline('sentiment-analysis', model = model, tokenizer = tokenizer)(message)
+    rating = pipeline('sentiment-analysis', model = model, tokenizer = tokenizer)(message)
 
     # print and return message
-    out = str(message) + " " + str(x)
+    out = str(message) + " " + str(rating)
     print(out)
-    
-    return out
+
+    return {
+        'statusCode': 200,
+        'body': str(out)
+    }
