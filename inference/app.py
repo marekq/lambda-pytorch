33b8034ba6ad0eb433ae5c57d8bfc73483eff515
Marek Kuczynski <marek.kuczynski@gmail.com>
2021-02-16 00:18:30 +0100

    added power tools

--- inference/app.py
+++ inference/app.py
@@ -1,10 +1,18 @@
 import base64
 from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
+from aws_lambda_powertools import Logger, Tracer
+
+# initialize powertools logger and tracer
+logger = Logger()
+tracer = Tracer()
 
 # load model and tokenizer from local disk
 model = AutoModelForSequenceClassification.from_pretrained('./model/', local_files_only = True)
 tokenizer = AutoTokenizer.from_pretrained('./model/', local_files_only = True)
 
+# lambda handler
+@logger.inject_lambda_context(log_event = True)
+@tracer.capture_lambda_handler
 def handler(event, context):
 
     # get post string
